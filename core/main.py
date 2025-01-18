9c707532ff86006dfe573ef9b07ff1ce3bc8d89f
fati1387 <saleheefatemeh1@gmail.com>
2025-01-18 17:37:03 +0330

    create bot

--- core/main.py
+++ core/main.py
@@ -1,71 +1,71 @@
 import telebot
-
+import os
 from telebot import types
 
-API_TOKEN = '7347901122:AAGPo8b_ur82gRxYfTBjf3JP1tWgTYAFkpU'
+API_TOKEN = "7347901122:AAGPo8b_ur82gRxYfTBjf3JP1tWgTYAFkpU"
 bot = telebot.TeleBot(API_TOKEN)
 
 user = {}
-course = {'spring': ['ccna','mcsa','python for beginner','photoshop'],
-          'summer':['ccna','mcsa','python for beginner','python for advanced'],
-          'autumn':['icdl','seller'],
-          'winter':['photoshop','c']}
+course = {"spring": ["ccna","mcsa","python for beginner","photoshop"],
+          "summer":["ccna","mcsa","python for beginner","python for advanced"],
+          "autumn":["icdl","seller"],
+          "winter":["photoshop","c"]}
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=["start"])
 def start_message(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True,input_field_placeholder='place celect one of our options')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True,input_field_placeholder="place celect one of our options")
     markup.add(
-        types.KeyboardButton('Courses'),
-        types.KeyboardButton('home'),
-        types.KeyboardButton('call with ac'),
+        types.KeyboardButton("Courses"),
+        types.KeyboardButton("home"),
+        types.KeyboardButton("call with ac"),
     )
     user = {}
-    bot.send_message(message.chat.id,'select one our options',reply_markup=markup)#
+    bot.send_message(message.chat.id,"select one our options",reply_markup=markup)#
 
 def see_course_season(message):
     
-    markup=types.ReplyKeyboardMarkup(resize_keyboard=True,input_field_placeholder='place celect one season',row_width=4)
-    markup.add(types.KeyboardButton('spring'),
-        types.KeyboardButton('summer'),
-        types.KeyboardButton('autumn'),
-        types.KeyboardButton('winter')
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True,input_field_placeholder="place celect one season",row_width=4)
+    markup.add(types.KeyboardButton("spring"),
+        types.KeyboardButton("summer"),
+        types.KeyboardButton("autumn"),
+        types.KeyboardButton("winter")
         
     )
-    bot.reply_to(message,'selecte option',reply_markup=markup)
+    bot.reply_to(message,"selecte option",reply_markup=markup)
 
 
 def call_with_ac(message):
-    bot.reply_to(message,'for call to ac give this nummber 09123456789')
+    bot.reply_to(message,"for call to ac give this nummber 09123456789")
 
 def home(message):
-    bot.reply_to(message,'place celect one of our options')
+    bot.reply_to(message,"place celect one of our options")
 
 def spring_courses(message):
     markup = types.InlineKeyboardMarkup()
-    for cr in course['spring']:
+    for cr in course["spring"]:
         markup.add(types.InlineKeyboardButton(cr , callback_data=cr))
-    bot.send_message(message.chat.id,'for register course click on it',reply_markup=markup)#
+    bot.send_message(message.chat.id,"for register course click on it",reply_markup=markup)#
 
 def summer_courses(message):
     markup = types.InlineKeyboardMarkup()
-    for cr in course['summer']:
+    for cr in course["summer"]:
         markup.add(types.InlineKeyboardButton(cr , callback_data=cr))
-    bot.send_message(message.chat.id,'for register course click on it',reply_markup=markup)#
+    bot.send_message(message.chat.id,"for register course click on it",reply_markup=markup)#
 
 def autumn_courses(message):
     markup = types.InlineKeyboardMarkup()
-    for cr in course['autumn']:
+    for cr in course["autumn"]:
         markup.add(types.InlineKeyboardButton(cr , callback_data=cr))
-    bot.send_message(message.chat.id,'for register course click on it',reply_markup=markup)#
+    bot.send_message(message.chat.id,"for register course click on it",reply_markup=markup)#
 
 def winter_courses(message):
     markup = types.InlineKeyboardMarkup()
-    for cr in course['winter']:
+    for cr in course["winter"]:
         markup.add(types.InlineKeyboardButton(cr , callback_data=cr))
-    bot.send_message(message.chat.id,'for register course click on it',reply_markup=markup)#
+    bot.send_message(message.chat.id,"for register course click on it",reply_markup=markup)#
 @bot.callback_query_handler(func=lambda call:True)
 def handler_register_course(call):
-    user['course']=call.data
+    user["course"]=call.data
     message=call.message
     text="""
     place insert your name and family
@@ -73,43 +73,42 @@ def handler_register_course(call):
     bot.reply_to(message,text)
     bot.register_next_step_handler(message,insert_basic_info)#
 def insert_basic_info(message):
-    user['info']=message.text
+    user["info"]=message.text
     text="""
-    place insert your phone nummber
+    place insert your phone number
     """
 
     bot.reply_to(message,text)
-    bot.register_next_step_handler(message,insert_phone_nummber)#
-def insert_phone_nummber(message):
-    user['phone']=message.text
+    bot.register_next_step_handler(message,insert_phone_number)#
+def insert_phone_number(message):
+    user["phone"]=message.text
     text="""
     pre_register complete successfully
     """
     bot.reply_to(message,text)
-    with open('./export/register.txt','a') as file:
+    with open("./export/register.txt","a") as file:
         
-        file.write(f'{user['season']}\n========\n{user['info']}\n===========\n{user['phone']}\n============\n{user['course']}\n===========\n' )
+        file.write(f"{user['season']}\n========\n{user['info']}\n===========\n{user['phone']}\n============\n{user['course']}\n===========\n" )
         start_message(message)
-
 @bot.message_handler(func=lambda message:True)
 def call_back(message):
-    if message.text == 'call with ac':
+    if message.text == "call with ac":
         call_with_ac(message)
-    elif message.text == 'Courses':
+    elif message.text == "Courses":
         see_course_season(message)
-    elif message.text == 'home':
+    elif message.text == "home":
         home(message)
-    elif message.text == 'spring':
-        user['season']=message.text
+    elif message.text == "spring":
+        user["season"]=message.text
         spring_courses(message)
-    elif message.text == 'summer':
-        user['season']=message.text
+    elif message.text == "summer":
+        user["season"]=message.text
         summer_courses(message)
-    elif message.text == 'autumn':
-        user['season']=message.text
+    elif message.text == "autumn":
+        user["season"]=message.text
         autumn_courses(message)
-    elif message.text == 'winter':
-        user['season']=message.text
+    elif message.text == "winter":
+        user["season"]=message.text
         winter_courses(message)
 
 
